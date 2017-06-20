865fb7e84363d4cc917c5b4c0fe7999f0df64faa
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-06-21 00:49:49 +0430

    implementing plane and sphere hit functions

--- plane.cpp
+++ plane.cpp
@@ -1,10 +1,20 @@
 #include "plane.h"
 #include "vec.h"
 #include "hit.h"
+#include "def.h"
+
 
 plane::plane(const vec &p_pos, const vec &p_norm)
  : pos(p_pos), norm(p_norm) { }
 
 bool plane::hits(const ray &p_ray, hit &p_hit) const {
-  return true;
+  double t = (this->pos - p_ray.o) * this->norm / (p_ray.d * this->norm);
+  if(t > EPSILON) {
+    p_hit.t = t;
+    p_hit.norm = this->norm;
+    p_hit.pos = p_ray.o + t * p_ray.d;
+    return true;
+  } else {
+    return false;
+  }
 }
--- sphere.cpp
+++ sphere.cpp
@@ -1,10 +1,31 @@
 #include "sphere.h"
 #include "vec.h"
 #include "hit.h"
+#include "def.h"
+#include <cmath>
 
 sphere::sphere(const vec &p_pos, const double &p_radius)
  : pos(p_pos), radius(p_radius) { }
 
 bool sphere::hits(const ray &p_ray, hit &p_hit) const {
-  return true;
+  double t;
+  vec temp = p_ray.o - this->pos;
+  double a = p_ray.d * p_ray.d;
+  double b = 2.0 * temp * p_ray.d;
+  double c = temp * temp - this->radius * this->radius;
+  double disc = b * b - 4 * a * c;
+  if(disc < 0.0)
+    return false;
+  else {
+    double e = sqrt(disc);
+    double denom = 2.0 * a;
+    t = (-b - e) / denom;
+    if(t > EPSILON) {
+      p_hit.t = t;
+      p_hit.norm = (temp + t * p_ray.d) / this->radius;
+      p_hit.pos = p_ray.o + t * p_ray.d;
+      return true;
+    }
+  }
+  return false;
 }
