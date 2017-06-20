cf3b0f208953f8e051c8a73563bbc4364a230afc
Keyvan M. Kambakhsh <keyvankambakhsh@gmail.com>
2017-06-21 00:46:43 +0430

    adding constants

---
+++ include/def.h
@@ -0,0 +1,16 @@
+#ifndef __DEF__
+#define __DEF__
+
+#define EPSILON (0.000000001d)
+
+// Colors
+const vec BLACK(0,0,0);
+const vec WHITE(1,1,1);
+const vec RED(1,0,0);
+const vec GREEN(0,1,0);
+const vec BLUE(0,0,1);
+const vec YELLOW(1,1,0);
+const vec MAGENTA(1,0,1);
+const vec CYAN(0,1,1);
+
+#endif
